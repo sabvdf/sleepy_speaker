c21cc1a95f369c0c2447b11bc32337c96227037c
Stephan van der Feest <stephan@vanderfeest.nl>
2021-08-27 13:50:57 +0200

    Cleans up first test version into functions; Adds config.yml.

--- sleepy.py
+++ sleepy.py
@@ -1,107 +1,81 @@
 # import the pygame module, so you can use it
 import pygame
+import yaml
+config = yaml.safe_load(open("config.yml"))
+version = "0.2.12"
+
+print("Sleepy Speaker V"+version);
+print("Loaded config");
+print("Default volume: "+str(config["default_volume"]));
+print("Resume last volume: "+str(config["resume_last_volume"]));
+
+volume = config["default_volume"]
+playing = False
+
+def play():
+    pygame.mixer.music.load("HeavyRain.ogg")
+    pygame.mixer.music.play(-1, 10.0, 2000)
+
+def stop():
+    playing = False
+    pygame.mixer.music.fadeout(2000)
+    
+
+def set_volume(vol):
+    if(vol < 0.0):
+        vol = 0.0
+    if(vol > 1.0):
+        vol = 1.0
+    volume = vol
+    pygame.mixer.music.set_volume(vol)
+    
+def play_at_volume(vol):
+    if not playing:
+        playing = True
+        play()
+    set_volume(vol)
+
 
 def main():
     pygame.init()
     
     running = True
     
-    volume = 0.5
-    playing = False
-    
     while running:
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_KP_ENTER:
                     if playing:
-                        playing = False
-                        pygame.mixer.music.fadeout(2000)
+                        stop()
                     else:
-                        playing = True
-                        volume = 0.5
-                        pygame.mixer.music.load("HeavyRain.ogg")
-                        pygame.mixer.music.set_volume(volume)
-                        pygame.mixer.music.play(-1, 10.0, 2000)
+                        if config["resume_last_volume"]:
+                            play_at_volume(volume)
+                        else:
+                            play_at_volume(config["default_volume"])
                 elif event.key == pygame.K_KP0:
-                    volume = 0.1
-                    if playing == False:
-                        playing = True
-                        pygame.mixer.music.load("HeavyRain.ogg")
-                        pygame.mixer.music.play(-1, 10.0, 2000)
-                    pygame.mixer.music.set_volume(volume)
+                    play_at_volume(0.1)
                 elif event.key == pygame.K_KP1:
-                    volume = 0.2
-                    if playing == False:
-                        playing = True
-                        pygame.mixer.music.load("HeavyRain.ogg")
-                        pygame.mixer.music.play(-1, 10.0, 2000)
-                    pygame.mixer.music.set_volume(volume)
+                    play_at_volume(0.2)
                 elif event.key == pygame.K_KP2:
-                    volume = 0.3
-                    if playing == False:
-                        playing = True
-                        pygame.mixer.music.load("HeavyRain.ogg")
-                        pygame.mixer.music.play(-1, 10.0, 2000)
-                    pygame.mixer.music.set_volume(volume)
+                    play_at_volume(0.3)
                 elif event.key == pygame.K_KP3:
-                    volume = 0.4
-                    if playing == False:
-                        playing = True
-                        pygame.mixer.music.load("HeavyRain.ogg")
-                        pygame.mixer.music.play(-1, 10.0, 2000)
-                    pygame.mixer.music.set_volume(volume)
+                    play_at_volume(0.4)
                 elif event.key == pygame.K_KP4:
-                    volume = 0.5
-                    if playing == False:
-                        playing = True
-                        pygame.mixer.music.load("HeavyRain.ogg")
-                        pygame.mixer.music.play(-1, 10.0, 2000)
-                    pygame.mixer.music.set_volume(volume)
+                    play_at_volume(0.5)
                 elif event.key == pygame.K_KP5:
-                    volume = 0.6
-                    if playing == False:
-                        playing = True
-                        pygame.mixer.music.load("HeavyRain.ogg")
-                        pygame.mixer.music.play(-1, 10.0, 2000)
-                    pygame.mixer.music.set_volume(volume)
+                    play_at_volume(0.6)
                 elif event.key == pygame.K_KP6:
-                    volume = 0.7
-                    if playing == False:
-                        playing = True
-                        pygame.mixer.music.load("HeavyRain.ogg")
-                        pygame.mixer.music.play(-1, 10.0, 2000)
-                    pygame.mixer.music.set_volume(volume)
+                    play_at_volume(0.7)
                 elif event.key == pygame.K_KP7:
-                    volume = 0.8
-                    if playing == False:
-                        playing = True
-                        pygame.mixer.music.load("HeavyRain.ogg")
-                        pygame.mixer.music.play(-1, 10.0, 2000)
-                        pygame.mixer.music.set_volume(volume)
+                    play_at_volume(0.8)
                 elif event.key == pygame.K_KP8:
-                    volume = 0.9
-                    if playing == False:
-                        playing = True
-                        pygame.mixer.music.load("HeavyRain.ogg")
-                        pygame.mixer.music.play(-1, 10.0, 2000)
-                    pygame.mixer.music.set_volume(volume)
+                    play_at_volume(0.9)
                 elif event.key == pygame.K_KP9:
-                    volume = 1.0
-                    if playing == False:
-                        playing = True
-                        pygame.mixer.music.load("HeavyRain.ogg")
-                        pygame.mixer.music.play(-1, 10.0, 2000)
-                    pygame.mixer.music.set_volume(volume)
+                    play_at_volume(1.0)
                 elif event.key == pygame.K_KP_PLUS:
-                    volume += 0.01
-                    if(volume > 1.0):
-                        volume = 1.0
-                    pygame.mixer.music.set_volume(volume)
+                    set_volume(volume + 0.01)
                 elif event.key == pygame.K_KP_MINUS:
-                    volume -= 0.01
-                    if(volume < 0.01):
-                        volume = 0.01
-                    pygame.mixer.music.set_volume(volume)
+                    set_volume(volume - 0.01)
             elif event.type == pygame.QUIT:
                 running = False
          
